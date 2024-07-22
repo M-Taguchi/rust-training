7cb651fb3156808cc57296b637841866aee486bb
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-07-23 08:48:13 +0900

    第5章

--- 5/rectangles/src/main.rs
+++ 5/rectangles/src/main.rs
@@ -31,16 +31,52 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
+// fn main() {
+//     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("rect1 is {:#?}", rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+//     println!("rect1 is {:#?}", rect1);
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&rect1)
+//     );
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+// fn main() {
+//     let rect1 = Rectangle { width: 30, height: 50 };
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         rect1.area()
+//     );
+// }
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+    let sq = Rectangle::square(3);
+
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
