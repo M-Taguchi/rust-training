c6063b090e400dc2d862cac796ef3d0d06b8a97b
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-07-30 08:52:01 +0900

    第7章

--- 7/restaurant/src/lib.rs
+++ 7/restaurant/src/lib.rs
@@ -29,6 +29,9 @@ mod front_of_house {
     }
 }
 
+// use crate::front_of_house::hosting;
+pub use self::front_of_house::hosting;
+
 // pub fn eat_at_restaurant() {
 //     crate::front_of_house::hosting::add_to_waitlist();
 
@@ -65,11 +68,49 @@ mod back_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+// pub fn eat_at_restaurant() {
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+//     let order1 = back_of_house::Appetizer::Soup;
+//     let order2 = back_of_house::Appetizer::Salad;
+// }
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
+
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut map = HashMap::new();
+//     map.insert(1, 2);
+// }
+
+// use std::fmt;
+// use std::io;
+
+// fn function1() => fmt::Result {}
+
+// fn function2() -> io::Result<()> {}
+
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn function1() -> Result {}
+
+fn function2() -> IoResult<()> {}
+
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+}
+
+// use std::{cmp::Ordering, io};
+use std::io::{self, Write};
+
+use std::collections::*;
