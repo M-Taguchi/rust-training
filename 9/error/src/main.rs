674564dbc40e9254261b87e5fed194e52958d9db
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-08-19 08:57:39 +0900

    第9章

--- 9/error/src/main.rs
+++ 9/error/src/main.rs
@@ -63,3 +63,41 @@ fn read_username_from_file() -> Result<String, io::Error> {
 
     Ok(s)
 }
+
+use std::net::IpAddr;
+
+let home: IpAddr = "127.0.0.1".parse().unwrap();
+
+loop {
+    let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+    if guess < 1 || guess > 100 {
+        println!("The secret number will be between 1 and 100.");
+        continue;
+    }
+
+    match guess.cmp(&secret_number) {}
+}
+
+pub strcut Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
