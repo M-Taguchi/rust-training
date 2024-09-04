1ae785903e4694295c8d7b108e60c2c05f595ed9
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-09-05 08:55:59 +0900

    第11章

--- 11/adder/src/lib.rs
+++ 11/adder/src/lib.rs
@@ -10,9 +10,9 @@ impl Rectangle {
     }
 }
 
-pub fn add_two(a: i32) -> i32 {
-    a + 2
-}
+// pub fn add_two(a: i32) -> i32 {
+//     a + 2
+// }
 
 pub fn greeting(name: &str) -> String {
     format!("Hello {}!", name)
@@ -40,6 +40,19 @@ impl Guess {
 
         Guess { value }
     }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -101,12 +114,48 @@ mod tests {
     //     Guess::new(200);
     // }
 
+    // #[test]
+    // fn it_works() -> Result<(), String> {
+    //     if 2 + 2 == 4 {
+    //         Ok(())
+    //     } else {
+    //         Err(String::from("two plus two does not four"))
+    //     }
+    // }
+
+    // #[test]
+    // fn this_test_will_pass() {
+    //     let value = prints_and_returns_10(4);
+    //     assert_eq!(10, value)
+    // }
+
+    // #[test]
+    // fn this_test_will_fail() {
+    //     let value = prints_and_returns_10(8);
+    //     assert_eq!(5, value)
+    // }
+
+    // #[test]
+    // fn add_two_and_two() {
+    //     assert_eq!(4, add_two(2));
+    // }
+
+    // #[test]
+    // fn add_three_and_two() {
+    //     assert_eq!(5, add_two(3));
+    // }
+
+    // #[test]
+    // fn one_hundred() {
+    //     assert_eq!(102, add_two(100));
+    // }
+
+    // #[test]
+    // #[ignore]
+    // fn expensive_test() {}
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not four"))
-        }
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
---
+++ 11/adder/target/debug/deps/integration_test-8b6b457d19e9c354.d
@@ -0,0 +1,5 @@
+/workspace/11/adder/target/debug/deps/integration_test-8b6b457d19e9c354: tests/integration_test.rs
+
+/workspace/11/adder/target/debug/deps/integration_test-8b6b457d19e9c354.d: tests/integration_test.rs
+
+tests/integration_test.rs:
