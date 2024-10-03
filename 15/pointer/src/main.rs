12e62499b7e35eb67e4294adb9529f73c8a3fa86
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-10-03 09:03:04 +0900

    一時コミット

--- 15/pointer/src/main.rs
+++ 15/pointer/src/main.rs
@@ -64,22 +64,47 @@ impl<T> Deref for MyBox<T> {
 //     println!("CustomSmartPointer dropped before the end of main.")
 // }
 
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+
+// use List::{Cons, Nil};
+// use std::rc::Rc;
+
+// fn main() {
+//     let a = Cons(5, Rc::new(Cons(10, Rc::new(Nil))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&b));
+//     {
+//         let c = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&c));
+//     }
+//     println!("count after goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
 use List::{Cons, Nil};
 use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
-    let a = Cons(5, Rc::new(Cons(10, Rc::new(Nil))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&b));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&c));
-    }
-    println!("count after goes out of scope = {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
