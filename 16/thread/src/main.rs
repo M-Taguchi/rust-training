74e76aa59eef690b29c53352233743b6d094c53f
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-10-08 08:59:42 +0900

    第16章

--- 16/thread/src/main.rs
+++ 16/thread/src/main.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
+use std::rc::Rc;
 
 // fn main() {
 //     let handle = thread::spawn(|| {
@@ -28,40 +30,98 @@ use std::sync::mpsc;
 
 //     handle.join().unwrap();
 // }
-fn main() {
-    let (tx, rx) = mpsc::channel();
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
 
-    let tx1 = mpsc::Sender::clone(&tx);
+//     let tx1 = mpsc::Sender::clone(&tx);
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
+//     thread::spawn(move || {
+//         let vals = vec![
+//             String::from("hi"),
+//             String::from("from"),
+//             String::from("the"),
+//             String::from("thread"),
+//         ];
 
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+//         for val in vals {
+//             tx1.send(val).unwrap();
+//             thread::sleep(Duration::from_secs(1));
+//         }
+//     });
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
+//     thread::spawn(move || {
+//         let vals = vec![
+//             String::from("more"),
+//             String::from("messages"),
+//             String::from("for"),
+//             String::from("you"),
+//         ];
     
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+//         for val in vals {
+//             tx.send(val).unwrap();
+//             thread::sleep(Duration::from_secs(1));
+//         }
+//     });
 
-    for received in rx {
-        println!("Got: {}", received);
+//     for received in rx {
+//         println!("Got: {}", received);
+//     }
+// }
+
+// fn main() {
+//     let m = Mutex::new(5);
+
+//     {
+//         let mut num = m.lock().unwrap();
+//         *num = 6;
+//     }
+
+//     println!("m = {:?}", m);
+// }
+
+// fn main() {
+//     let counter = Mutex::new(0);
+//     let mut handles = vec![];
+
+//     let handle = thread::spawn(move || {
+//         let mut num = counter.lock().unwrap();
+
+//         *num += 1;
+//     });
+
+//     handles.push(handle);
+
+//     let handle2 = thread::spawn(move || {
+//         let mut num2 = counter.lock().unwrap();
+
+//         *num2 += 1;
+//     });
+//     handles.push(handle2);
+
+//     for handle in handles {
+//         handle.join().unwrap();
+//     }
+
+//     println!("Result: {}", *counter.lock().unwrap());
+// }
+
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
