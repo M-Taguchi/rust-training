d29f85465f9ddf7814eef87a15343b1cb2727e96
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-10-22 08:44:29 +0900

    第17章

--- 17/object/src/main.rs
+++ 17/object/src/main.rs
@@ -12,25 +12,46 @@ impl Draw for SelectBox {
     fn draw(&self) {}
 }
 
-fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("MayBe"),
-                    String::from("No")
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
+// fn main() {
+//     let screen = Screen {
+//         components: vec![
+//             Box::new(SelectBox {
+//                 width: 75,
+//                 height: 10,
+//                 options: vec![
+//                     String::from("Yes"),
+//                     String::from("MayBe"),
+//                     String::from("No")
+//                 ],
+//             }),
+//             Box::new(Button {
+//                 width: 50,
+//                 height: 10,
+//                 label: String::from("OK"),
+//             }),
+//         ],
+//     };
 
-    screen.run();
+//     screen.run();
+// }
+
+extern crate blog;
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    // post.request_review();
+    // assert_eq!("", post.content());
+
+    // post.approve();
+
+    let post = post.request_review();
+
+    let post = post.approve();
+    
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
