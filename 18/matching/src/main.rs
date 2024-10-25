663446874d5b7c8ae51c14307267bd780c95ddc0
ra-tgc <masaki.taguchi@rakus.co.jp>
2024-10-25 09:00:29 +0900

    一時コミット

--- 18/matching/src/main.rs
+++ 18/matching/src/main.rs
@@ -1,44 +1,125 @@
-fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
+// fn main() {
+//     let favorite_color: Option<&str> = None;
+//     let is_tuesday = false;
+//     let age: Result<u8, _> = "34".parse();
 
-    if let Some(color) = favorite_color {
-        println!("Using your favorite color, {}, as the background", color);
-    } else if is_tuesday {
-        println!("Thuesday is green day!");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as the background color");
-        } else {
-            println!("Using Orange as the background color");
-        }
-    } else {
-        println!("Using blue as the background color");
-    }
+//     if let Some(color) = favorite_color {
+//         println!("Using your favorite color, {}, as the background", color);
+//     } else if is_tuesday {
+//         println!("Thuesday is green day!");
+//     } else if let Ok(age) = age {
+//         if age > 30 {
+//             println!("Using purple as the background color");
+//         } else {
+//             println!("Using Orange as the background color");
+//         }
+//     } else {
+//         println!("Using blue as the background color");
+//     }
 
-    let mut stack = Vec::new();
+//     let mut stack = Vec::new();
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+//     stack.push(1);
+//     stack.push(2);
+//     stack.push(3);
 
-    while let Some(top) = stack.pop() {
-        println!("{}", top);
-    }
+//     while let Some(top) = stack.pop() {
+//         println!("{}", top);
+//     }
 
-    let v = vec!['a', 'b', 'c'];
+//     let v = vec!['a', 'b', 'c'];
 
-    for (index, value) in v.iter().enumerate() {
-        println!("{} is at index {}", value, index);
-    }
+//     for (index, value) in v.iter().enumerate() {
+//         println!("{} is at index {}", value, index);
+//     }
 
-    let (x, y, z) = (1, 2, 3);
+//     let (x, y, z) = (1, 2, 3);
 
-    let point = (3, 5);
-    print_coordinates(&point);
+//     let point = (3, 5);
+//     print_coordinates(&point);
+// }
+
+// fn print_coordinates(&(x, y): &(i32, i32)) {
+//     println!("Current location: ({}, {})", x, y);
+// }
+
+// fn main() {
+//     // let x = Some(5);
+//     // let y = 10;
+
+//     // match x {
+//     //     Some(50) => println!("Got 50"),
+//     //     Some(y) => println!("Matched, y = {:?}", y),
+//     //     _ => println!("Default case, x = {:?}", x),
+//     // }
+
+//     // println!("at the end: x = {:?}, y = {:?}", x, y);
+
+//     // let x = 1;
+
+//     // match x {
+//     //     1 | 2 => println!("one or two"),
+//     //     3 => println!("three"),
+//     //     _ => println!("anything"),
+//     // }
+
+//     // match x {
+//     //     1..5 => println!("one through five"),
+//     //     _ => println!("something else"),
+//     // }
+
+//     // let x = 'c';
+//     // match x {
+//     //     'a'..'j' => println!("early ASCII letter"),
+//     //     'k'..'z' => println!("late ASCII letter"),
+//     //     _ => println!("something else"),
+//     // }
+
+// }
+
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-fn print_coordinates(&(x, y): &(i32, i32)) {
-    println!("Current location: ({}, {})", x, y);
+// fn main() {
+//     let p = Point { x: 0, y: 7 };
+
+//     // let Point { x: a, y: b } = p;
+//     // assert_eq!(0, a);
+//     // assert_eq!(7, b);
+    
+//     // let Point { x, y } = p;
+//     // assert_eq!(0, x);
+//     // assert_eq!(7, y);
+
+//     match p {
+//         Point { x, y: 0 } => println!("On the x axis at {}", x),
+//         Point { x: 0, y } => println!("On the y axis at {}", y),
+//         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+//     }
+// }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        },
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        },
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+    }
 }
